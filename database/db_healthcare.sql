87b74b43f8973ab4b6967500b72f804795a0732c
“Sidath” <sidathsanjaya.18@business.mrt.ac.lk>
2021-11-11 00:36:39 +0530

    .

--- database/db_healthcare.sql
+++ database/db_healthcare.sql
@@ -45,6 +45,79 @@ INSERT INTO `doctor` (`icDoctor`, `password`, `doctorId`, `doctorFirstName`, `do
 
 
 
+/* Create a Doctors' shedule table */
+
+CREATE TABLE `doctorschedule` (
+  `scheduleId` int(11) NOT NULL,
+  `scheduleDate` date NOT NULL,
+  `scheduleDay` varchar(15) NOT NULL,
+  `startTime` time NOT NULL,
+  `endTime` time NOT NULL,
+  `bookAvail` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+INSERT INTO `doctorschedule` (`scheduleId`, `scheduleDate`, `scheduleDay`, `startTime`, `endTime`, `bookAvail`) VALUES
+(40, '2015-12-13', 'Sunday', '09:00:00', '10:00:00', 'notavail'),
+(41, '2015-12-13', 'Sunday', '10:00:00', '11:00:00', 'available'),
+(42, '2015-12-13', 'Sunday', '11:00:00', '12:00:00', 'available'),
+(43, '2015-12-14', 'Monday', '11:00:00', '12:00:00', 'available'),
+(44, '2015-12-13', 'Sunday', '01:00:00', '02:00:00', 'available');
+
+
+
+CREATE TABLE `patient` (
+  `icPatient` bigint(12) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `patientFirstName` varchar(20) NOT NULL,
+  `patientLastName` varchar(20) NOT NULL,
+  `patientMaritialStatus` varchar(10) NOT NULL,
+  `patientDOB` date NOT NULL,
+  `patientGender` varchar(10) NOT NULL,
+  `patientAddress` varchar(100) NOT NULL,
+  `patientPhone` varchar(15) NOT NULL,
+  `patientEmail` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+INSERT INTO `patient` (`icPatient`, `password`, `patientFirstName`, `patientLastName`, `patientMaritialStatus`, `patientDOB`, `patientGender`, `patientAddress`, `patientPhone`, `patientEmail`) VALUES
+(920517105553, '123', 'Sidath', 'jaywardhana', 'single', '199-08-29', 'male', 'NO 153 milla road katubadda', '173567758', 'ssj@gmail.com');
+
+
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`appId`),
+  ADD UNIQUE KEY `scheduleId_2` (`scheduleId`),
+  ADD KEY `patientIc` (`patientIc`),
+  ADD KEY `scheduleId` (`scheduleId`);
+
+
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`icDoctor`);
+
+
+ALTER TABLE `doctorschedule`
+  ADD PRIMARY KEY (`scheduleId`);
+
+
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`icPatient`);
+
+
+ALTER TABLE `appointment`
+  MODIFY `appId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+
+
+ALTER TABLE `doctorschedule`
+  MODIFY `scheduleId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_4` FOREIGN KEY (`patientIc`) REFERENCES `patient` (`icPatient`),
+  ADD CONSTRAINT `appointment_ibfk_5` FOREIGN KEY (`scheduleId`) REFERENCES `doctorschedule` (`scheduleId`);
+COMMIT;
+
 
 
 
