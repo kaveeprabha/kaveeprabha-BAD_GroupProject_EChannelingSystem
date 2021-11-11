5a2df70545562315e60add28652676d382dacc3d
Kaveesha <kaveesha344@gmail.com>
2021-11-10 23:04:45 -0800

    .

--- database/db_healthcare.sql
+++ database/db_healthcare.sql
@@ -5,7 +5,7 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
+/*Appointment table created*/
 
 
 CREATE TABLE `appointment` (
@@ -21,6 +21,28 @@ CREATE TABLE `appointment` (
 
 INSERT INTO `appointment` (`appId`, `patientIc`, `scheduleId`, `appSymptom`, `appComment`, `status`) VALUES
 (86, 920517105553, 40, 'Headache', '-', 'done');
+
+/*Doctor table created*/
+
+CREATE TABLE `doctor` (
+  `icDoctor` bigint(12) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `doctorId` varchar(10) NOT NULL,
+  `doctorFirstName` varchar(50) NOT NULL,
+  `doctorLastName` varchar(50) NOT NULL,
+  `doctorAddress` varchar(100) NOT NULL,
+  `doctorPhone` varchar(15) NOT NULL,
+  `doctorEmail` varchar(20) NOT NULL,
+  `doctorDOB` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/* Two doctors who rigestered with suwaseth were added to the table */
+
+INSERT INTO `doctor` (`icDoctor`, `password`, `doctorId`, `doctorFirstName`, `doctorLastName`, `doctorAddress`, `doctorPhone`, `doctorEmail`, `doctorDOB`) VALUES
+(123456789, '123', 'Nathasha', 'Dr.Nathasha', 'Singhawansha', 'Darmaraja mawatha Benthara', '+94173567758', 'nathasha123@gmail.com', '1990-04-10');
+INSERT INTO `doctor` (`icDoctor`, `password`, `doctorId`, `doctorFirstName`, `doctorLastName`, `doctorAddress`, `doctorPhone`, `doctorEmail`, `doctorDOB`) VALUES
+(123456788, '123', 'Hasitha', 'Dr.Hasitha', 'perera', 'Darmaraja mawatha Benthara', '+94173567725', 'hasitha12@gmail.com', '1990-06-12');
+
 
 
 
